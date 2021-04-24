33901e0024e292942c55f85839a9edf413f6cb21
Ana Marca <a2019113660@estudiantes.upsa.edu.bo>
2021-04-24 10:58:54 -0400

    Continuacion modelo

--- Parcial2_AnaMarca/Parcial2_AnaMarca/Models/Friend.cs
+++ Parcial2_AnaMarca/Parcial2_AnaMarca/Models/Friend.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace Parcial2_AnaMarca.Models
 {
     public class Friend
     {
+        [Key]
+        public int FriendId { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+        public string Nickname { get; set; }
+        public string Birthday { get; set; }
+
     }
 }
